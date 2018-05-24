ad24b1331e336e0834cfa7d202549b0a8f8cbda8
meeqi <373162947@qq.com>
2018-05-24 10:20:13 +0800

    单例

---
+++ factory/Factory.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: MeeQi
+ * Date: 2018/5/24
+ * Time: 上午10:21
+ */
---
+++ single/run.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: MeeQi
+ * Date: 2018/5/24
+ * Time: 上午10:14
+ */
+
+require 'Single.php';
+
+Single::getInstance();
+Single::getInstance();
+Single::getInstance();
